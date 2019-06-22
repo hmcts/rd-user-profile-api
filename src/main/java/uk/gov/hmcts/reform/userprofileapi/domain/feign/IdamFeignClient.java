73cd5dd850b136c35cb8af985b236c0258611c0b
shreedhar-hmcts <shreedhar.lomte@hmcts.net>
2019-06-22 13:46:34 +0100

    RDCC-252 Replaced local ip with localhost

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/feign/IdamFeignClient.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/feign/IdamFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "IdamFeignClient", url = "172.29.14.129:8888")
+@FeignClient(name = "IdamFeignClient", url = "localhost:8888")
 public interface IdamFeignClient {
 
 
