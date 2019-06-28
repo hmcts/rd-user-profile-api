3a065b8c33dabf0f98b885d9e13dd234b0543696
Mark Nuthalapati <mark.nuthalapati@gmail.com>
2019-06-28 20:28:10 +0100

    Changing main tf file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,8 +16,7 @@ locals {
   s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
   s2s_vault_name = "s2s-${local.local_env}"
   s2s_vault_uri = "https://s2s-${local.local_env}.vault.azure.net/"
-
-  idam_url = "https://idam-api.${local.local_env}.platform.hmcts.net"
+  idam_url = "${var.env == "prod" ? "https://idam-api.platform.hmcts.net" : "https://idam-api.${local.local_env}.platform.hmcts.net" }"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -36,51 +35,46 @@ data "azurerm_key_vault" "s2s_key_vault" {
   resource_group_name = "rpe-service-auth-provider-${local.local_env}"
 }
 
-data "azurerm_key_vault_secret" "up_s2s_secret" {
-  name = "up-s2s-secret"
-  vault_uri = "${data.azurerm_key_vault.rd_key_vault.vault_uri}"
-}
-
 data "azurerm_key_vault_secret" "up_s2s_microservice" {
   name = "up-s2s-microservice"
-  vault_uri = "${data.azurerm_key_vault.rd_key_vault.vault_uri}"
-}
-
-data "azurerm_key_vault_secret" "idam_url" {
-  name = "idam-url"
-  vault_uri = "${data.azurerm_key_vault.rd_key_vault.vault_uri}"
+  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
 
 data "azurerm_key_vault_secret" "s2s_url" {
   name = "s2s-url"
-  vault_uri = "${data.azurerm_key_vault.rd_key_vault.vault_uri}"
+  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "DB_UP_USERNAME" {
+data "azurerm_key_vault_secret" "up_s2s_secret" {
+  name = "microservicekey-rd-user-profile-api"
+  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
+}
+
+resource "azurerm_key_vault_secret" "POSTGRES-USER" {
   name      = "${var.component}-POSTGRES-USER"
   value     = "${module.db-user-profile.user_name}"
   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "DB_UP_PASSWORD" {
+resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name      = "${var.component}-POSTGRES-PASS"
   value     = "${module.db-user-profile.postgresql_password}"
   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "DB_UP_POSTGRES_HOST" {
+resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name      = "${var.component}-POSTGRES-HOST"
   value     = "${module.db-user-profile.host_name}"
   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "DB_UP_POSTGRES_PORT" {
+resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   name      = "${var.component}-POSTGRES-PORT"
   value     = "5432"
   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
 }
 
-resource "azurerm_key_vault_secret" "DB_UP_POSTGRES_DATABASE" {
+resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name      = "${var.component}-POSTGRES-DATABASE"
   value     = "${module.db-user-profile.postgresql_database}"
   key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
@@ -95,7 +89,6 @@ module "db-user-profile" {
   postgresql_user = "dbuserprofile"
   database_name = "dbuserprofile"
   common_tags = "${var.common_tags}"
-
 }
 
 module "rd-user-profile-api" {
@@ -125,15 +118,12 @@ module "rd-user-profile-api" {
     DB_UP_PASSWORD = "${module.db-user-profile.postgresql_password}"
     DB_UP_CONNECTION_OPTIONS = "?"
 
-    UP_S2S_SECRET = "${data.azurerm_key_vault_secret.up_s2s_secret.value}"
-    UP_S2S_MICROSERVICE = "${data.azurerm_key_vault_secret.up_s2s_microservice.value}"
-
-    IDAM_URL = "${data.azurerm_key_vault_secret.idam_url.value}"
-    S2S_URL = "${data.azurerm_key_vault_secret.s2s_url.value}"
+    IDAM_URL = "${local.idam_url}"
+    S2S_URL = "${local.s2s_url}"
 
     ROOT_LOGGING_LEVEL = "${var.root_logging_level}"
     LOG_LEVEL_SPRING_WEB = "${var.log_level_spring_web}"
-    LOG_LEVEL_IA = "${var.log_level_rd}"
+    LOG_LEVEL_RD = "${var.log_level_rd}"
     EXCEPTION_LENGTH = 100
   }
 }
