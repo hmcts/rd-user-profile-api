026bf54bc72b9c7c2d8875bee92acf06c2afe0a6
shreedhar-hmcts <shreedhar.lomte@hmcts.net>
2019-06-28 14:04:14 +0100

    RDCC-319 Removed vulnerability

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/infrastructure/controllers/advice/UserProfileControllerAdvice.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/infrastructure/controllers/advice/UserProfileControllerAdvice.java
@@ -105,7 +105,6 @@ public class UserProfileControllerAdvice {
     private ResponseEntity<Object> errorDetailsResponseEntity(Exception ex, HttpStatus httpStatus, String errorMsg) {
 
         log.error(LOG_STRING, ex.getMessage());
-        log.error(LOG_STRING, ex.getStackTrace());
         ErrorResponse errorDetails = ErrorResponse.builder()
                 .errorMessage(errorMsg)
                 .errorDescription(getRootException(ex).getLocalizedMessage())
