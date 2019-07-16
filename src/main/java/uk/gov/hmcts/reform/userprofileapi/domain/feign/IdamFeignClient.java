cb40268bd30fb9e22181313412c3783b232a2078
Mark Nuthalapati <mark.nuthalapati@hmcts.net>
2019-07-16 11:45:25 +0100

    Adding explicit feign interceptor config to Idam

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/feign/IdamFeignClient.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/feign/IdamFeignClient.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import uk.gov.hmcts.reform.userprofileapi.config.FeignInterceptorConfiguration;
 
-@FeignClient(name = "IdamFeignClient", url = "${auth.idam.client.baseUrl}")
+@FeignClient(name = "IdamFeignClient", url = "${auth.idam.client.baseUrl}", configuration = FeignInterceptorConfiguration.class)
 public interface IdamFeignClient {
 
     @RequestMapping(method = RequestMethod.POST, value = "/user/registration")
