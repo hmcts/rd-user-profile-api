1f37746c8e4fc436a35d165dcea32d22f44653a9
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2019-09-11 11:06:26 +0100

    RDCC-451:adding new user response for updateRoles scenario.

---
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/client/UserProfileRolesResponse.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.userprofileapi.client;
+
+import static uk.gov.hmcts.reform.userprofileapi.util.IdamStatusResolver.resolveStatusAndReturnMessage;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserProfileRolesResponse {
+
+    private HttpStatus responseStatusCode;
+    private String statusMessage;
+
+    public UserProfileRolesResponse(HttpStatus idamGetResponseStatusCode) {
+        loadStatusCodes(idamGetResponseStatusCode);
+    }
+
+    private void loadStatusCodes(HttpStatus idamGetResponseStatusCode) {
+        this.responseStatusCode = idamGetResponseStatusCode;
+        this.statusMessage = resolveStatusAndReturnMessage(idamGetResponseStatusCode);
+    }
+}
+
