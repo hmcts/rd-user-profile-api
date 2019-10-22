e1d6fe0c1f9c32ad24a27a3c133e0e1dc3f5000a
akiohmcts <akio.cox@hmcts.net>
2019-10-22 23:12:28 +0100

    RDCC-418 adding unit test coverage

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/AttributeResponse.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/AttributeResponse.java
@@ -3,14 +3,10 @@ package uk.gov.hmcts.reform.userprofileapi;
 import static uk.gov.hmcts.reform.userprofileapi.util.IdamStatusResolver.resolveStatusAndReturnMessage;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
 
 @Getter
-@Setter
-@NoArgsConstructor
 public class AttributeResponse {
 
     private Integer idamStatusCode;
