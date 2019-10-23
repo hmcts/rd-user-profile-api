e977d457ff7224bf6bdb8902ef829c7fed8f30eb
akiohmcts <akio.cox@hmcts.net>
2019-10-23 10:42:23 +0100

    RDCC-418 checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/service/impl/AuditServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/service/impl/AuditServiceImpl.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.userprofileapi.service.impl;
 
+import static uk.gov.hmcts.reform.userprofileapi.util.IdamStatusResolver.resolveStatusAndReturnMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -8,8 +10,6 @@ import uk.gov.hmcts.reform.userprofileapi.domain.entities.Audit;
 import uk.gov.hmcts.reform.userprofileapi.domain.entities.UserProfile;
 import uk.gov.hmcts.reform.userprofileapi.repository.AuditRepository;
 import uk.gov.hmcts.reform.userprofileapi.service.AuditService;
-
-import static uk.gov.hmcts.reform.userprofileapi.util.IdamStatusResolver.resolveStatusAndReturnMessage;
 
 @Service
 public class AuditServiceImpl implements AuditService {
