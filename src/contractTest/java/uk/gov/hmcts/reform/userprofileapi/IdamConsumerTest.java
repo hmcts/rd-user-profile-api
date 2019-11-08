72ed6f0fe891ed21a416bc4392fba34c23b4cc00
ConorH994 <conor.halpin@hmcts.net>
2019-11-08 16:45:17 +0000

    added one test for registration response

--- src/contractTest/java/uk/gov/hmcts/reform/userprofileapi/IdamConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/userprofileapi/IdamConsumerTest.java
@@ -18,6 +18,7 @@ import io.restassured.parsing.Parser;
 
 import java.util.Map;
 
+import io.restassured.response.Response;
 import net.serenitybdd.rest.SerenityRest;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,6 +39,7 @@ public class IdamConsumerTest {
 
     private static final String IDAM_DETAILS_URL = "/details";
     private static final String IDAM_GET_USER_URL = "/api/v1/users/.*";
+    private static final String IDAM_POST_USER_REGISTRATION_URL = "/api/v1/users/registration";
     private static final String ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiRm8rQXAybThDT3ROb290ZjF4TWg0bGc3MFlBPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJmcmVnLXRlc3QtdXNlci1ZOHlqVURSeWpyQGZlZW1haWwuY29tIiwiYXV0aF9sZXZlbCI6MCwiYXVkaXRUcmFja2luZ0lkIjoiYTU2MDliYjYtYzEzYi00MjQ0LTg3ODItNDNmZGViMDZlMDBjIiwiaXNzIjoiaHR0cHM6Ly9mb3JnZXJvY2stYW0uc2VydmljZS5jb3JlLWNvbXB1dGUtaWRhbS1hYXQuaW50ZXJuYWw6ODQ0My9vcGVuYW0vb2F1dGgyL2htY3RzIiwidG9rZW5OYW1lIjoiYWNjZXNzX3Rva2VuIiwidG9rZW5fdHlwZSI6IkJlYXJlciIsImF1dGhHcmFudElkIjoiYWNjNmUyYTAtMWExYi00OGM3LWJmZGItNzI1NjllM2E1NjkzIiwiYXVkIjoicmQtcHJvZmVzc2lvbmFsLWFwaSIsIm5iZiI6MTU2OTQ0MTkxMSwiZ3JhbnRfdHlwZSI6ImF1dGhvcml6YXRpb25fY29kZSIsInNjb3BlIjpbIm9wZW5pZCIsInByb2ZpbGUiLCJyb2xlcyIsImNyZWF0ZS11c2VyIiwibWFuYWdlLXVzZXIiXSwiYXV0aF90aW1lIjoxNTY5NDQxOTExMDAwLCJyZWFsbSI6Ii9obWN0cyIsImV4cCI6MTU2OTQ1NjMxMSwiaWF0IjoxNTY5NDQxOTExLCJleHBpcmVzX2luIjoxNDQwMCwianRpIjoiY2Q5MWM0NjQtMzU0Zi00N2I2LTkwYTUtNWY2Y2U3NGUwYTY5In0.aLobAYYCxkmryzKV1stmag63h-ndxrDjO4462YERcLDIXVmvFJNXfdPRg9U8WGv0GkOrSkHVJ7tbdLQySnOVYulXkPl71g5MqU7ZuEQvHaBpfW9exBCfP-pw8kWyMUck-rB00tkEX7ZpS6euQM0WVbdczPnClxR3tWwktPfN-bCo6PPwqiMkC1DgTmjQBMtjgP1nEiJM7Kocqb2X3OCItf4lps1_nSG68jI98fwaLn8WQgk1sw9eebskChXDfpmIyreeGFWpHNpdFqOFfYEC5FnSgXHQw7Eu-hc5RofPZzKFrbwZHC31t5guK9Wq8zn9Xwe6743g4ozm3EHN8fsjVQ"; //todo
 
     @BeforeEach
@@ -185,5 +187,47 @@ public class IdamConsumerTest {
                 .stringType("email", "jack@spookmail.com")
                 .booleanType("pending", false)
                 .object("roles", array);
+    }
+
+    @Test
+    @Pact(provider = "Idam_api", consumer = "rd_user_profile_api__idam_api")
+    public RequestResponsePact executePostRegistrationAndGet201(PactDslWithProvider builder) {
+
+        Map<String, String> headers = Maps.newHashMap();
+        headers.put(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN);
+
+        return builder
+                .given("Idam successfully returns 201 Created status")
+                .uponReceiving("Provider receives a POST /registration request from an RD - USER PROFILE API")
+                .path(IDAM_POST_USER_REGISTRATION_URL)
+                .method(HttpMethod.POST.toString())
+                .headers(headers)
+                .willRespondWith()
+                .status(HttpStatus.CREATED.value())
+                .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "executePostRegistrationAndGet201")
+    public void should_post_for_registration_and_return_201(MockServer mockServer) throws JSONException {
+
+        Map<String, String> headers = Maps.newHashMap();
+        headers.put(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN);
+
+        Response actualResponseBody =
+                SerenityRest
+                        .given()
+                        .headers(headers)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                        .when()
+                        .post(mockServer.getUrl() + IDAM_POST_USER_REGISTRATION_URL)
+                        .then()
+                        .statusCode(201)
+                        .and()
+                        .extract()
+                        .response();
+
+        //JSONObject response = new JSONObject(actualResponseBody);
+        assertThat(actualResponseBody.getStatusCode()).isEqualTo(201);
     }
 }
