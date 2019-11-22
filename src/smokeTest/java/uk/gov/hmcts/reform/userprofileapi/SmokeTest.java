3004927c973baaabd90a9f3042a1d8517a099a73
jamesohmcts <james.odukoya@hmcts.net>
2019-11-22 12:53:31 +0000

    "RDCC-705 enabled smoke test for user profile"

--- src/smokeTest/java/uk/gov/hmcts/reform/userprofileapi/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/userprofileapi/SmokeTest.java
@@ -1,36 +1,46 @@
 package uk.gov.hmcts.reform.userprofileapi;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
-import org.springframework.http.HttpStatus;
+import org.junit.runner.RunWith;
 
+@RunWith(SpringIntegrationSerenityRunner.class)
 public class SmokeTest {
 
-    private final String targetInstance =
-        StringUtils.defaultIfBlank(
-            System.getenv("TEST_URL"),
-            "http://localhost:8091"
-        );
+    private final String targetInstance = "http://rd-user-profile-api-aat.service.core-compute-aat.internal";
+    /* StringUtils.defaultIfBlank(
+                    System.getenv("TEST_URL"),
+                    "http://localhost:8090"
+    );*/
 
     @Test
     public void should_prove_app_is_running_and_healthy() {
+        // local test
+        SerenityRest.proxy("proxyout.reform.hmcts.net", 8080);
+        RestAssured.proxy("proxyout.reform.hmcts.net", 8080);
 
         RestAssured.baseURI = targetInstance;
         RestAssured.useRelaxedHTTPSValidation();
 
-        String response = SerenityRest
-            .when()
-            .get("/health")
-            .then()
-            .statusCode(HttpStatus.OK.value())
-            .and()
-            .extract().body().asString();
+        Response response = RestAssured
+                .given()
+                .relaxedHTTPSValidation()
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .get("/")
+                .andReturn();
 
-        assertThat(response)
-            .contains("UP");
+        if (null != response.body()) {
+            assertThat(response.body().asString())
+                    .contains("Welcome to the User Profile API");
+
+        }
+
     }
 }
