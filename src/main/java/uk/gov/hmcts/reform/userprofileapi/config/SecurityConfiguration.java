b4794eb9fc0c51b57d4f6c55e91b8097de453a6a
abhijit <Abhijit.diwan@hmcts.net>
2019-12-02 16:30:57 +0000

    test: testing logs

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/config/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/config/SecurityConfiguration.java
@@ -5,12 +5,10 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +17,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import uk.gov.hmcts.reform.auth.checker.core.RequestAuthorizer;
 import uk.gov.hmcts.reform.auth.checker.core.service.Service;
 import uk.gov.hmcts.reform.auth.checker.core.user.User;
@@ -33,8 +32,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final List<String> anonymousPaths = new ArrayList<>();
     private AuthCheckerServiceAndUserFilter authCheckerServiceAndUserFilter;
 
-    @Autowired
-    private HttpServletRequest request;
+
 
     private static final Logger LOG = LoggerFactory.getLogger(SecurityConfiguration.class);
 
@@ -45,15 +43,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                                  AuthenticationManager authenticationManager) {
 
 
-        //print all headers
-        LOG.info("::Authorization header idam token filter::" + request.getHeader("Authorization"));
-        LOG.info("::Authorization header S2S Token s2s token in filter::" + request.getHeader("ServiceAuthorization"));
+
+
+        LOG.info("::Authorization header idam token filter credentials::" + SecurityContextHolder.getContext().getAuthentication().getCredentials());
+        LOG.info("::Authorization header idam token filter name::" + SecurityContextHolder.getContext().getAuthentication().getName());
 
         //inside security configuration
 
         authCheckerServiceAndUserFilter = new AuthCheckerServiceAndUserFilter(serviceRequestAuthorizer, userRequestAuthorizer);
 
         authCheckerServiceAndUserFilter.setAuthenticationManager(authenticationManager);
+
+
 
         //print all headers
         LOG.info("::Authorization header done in filter");
