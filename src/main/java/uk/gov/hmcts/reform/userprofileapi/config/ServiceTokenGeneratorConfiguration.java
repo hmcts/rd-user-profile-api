273f0656dbeb8077058a8ba926f13b209abcd534
abhijit <Abhijit.diwan@hmcts.net>
2019-12-03 16:48:03 +0000

    test: testing logs

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/config/ServiceTokenGeneratorConfiguration.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.userprofileapi.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,12 +14,18 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 @Lazy
 public class ServiceTokenGeneratorConfiguration {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ServiceTokenGeneratorConfiguration.class);
+
     @Bean
     public AuthTokenGenerator authTokenGenerator(
-        @Value("${idam.s2s-auth.totp_secret}") String secret,
-        @Value("${idam.s2s-auth.microservice}") String microService,
-        ServiceAuthorisationApi serviceAuthorisationApi
+            @Value("${idam.s2s-auth.totp_secret}") String secret,
+            @Value("${idam.s2s-auth.microservice}") String microService,
+            ServiceAuthorisationApi serviceAuthorisationApi
     ) {
+
+        LOG.info(String.format("::totpSecret in ServiceTokenGeneratorConfiguration:: %S", secret));
+        LOG.info(String.format("::s2sAuthMicroservicename in ServiceTokenGeneratorConfiguration:: %S", microService));
+
         return AuthTokenGeneratorFactory.createDefaultGenerator(
             secret,
             microService,
--- src/main/java/uk/gov/hmcts/reform/userprofileapi/controller/TestFilter.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/controller/TestFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
@@ -19,6 +20,12 @@ import org.springframework.web.filter.GenericFilterBean;
 public class TestFilter extends GenericFilterBean {
 
     private static final Logger LOG = LoggerFactory.getLogger(TestFilter.class);
+
+    @Value("${idam.s2s-auth.totp_secret}")
+    String totpSecret;
+
+    @Value("${idam.s2s-auth.microservice}")
+    String s2sAuthMicroservicename;
 
     @Override
     public void doFilter(
@@ -34,23 +41,15 @@ public class TestFilter extends GenericFilterBean {
         param1 = Objects.isNull(param1) ? "" : param1.replaceAll("[\n|\r|\t]", "_");
         param2 = Objects.isNull(param2) ? "" : param2.replaceAll("[\n|\r|\t]", "_");
 
+        LOG.info(String.format("::totpSecret:::: %S", totpSecret));
+        LOG.info(String.format("::s2sAuthMicroservicename:: %S", s2sAuthMicroservicename));
+
         LOG.info(String.format(":Authorization Header from swagger:: %S", param1));
-        LOG.info(String.format(":ServiceAuthorization from swagger:: %S", param2));
+        LOG.info(String.format(":Authorization Header from swagger:: %S", param1));
+
 
         MutableHttpServletRequest mutableRequest = new MutableHttpServletRequest((HttpServletRequest) request);
-        mutableRequest.putHeader("Authorization", "eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiS0N4QmRlaHNIVUY2OTc4U2l6dklTRXhjWDBFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJhYmhpaml0QGdtYWlsLmNvbSIsImF1dGhfbGV2ZWwiOjAsImF1ZGl0VHJhY2tpbmdJZCI6ImQzNTc4ODA1LWEzY2EtNGZhOC1hZDAyLWRjMzcyOTg0ZmRjZCIsImlzcyI6Imh0dHBzOi8vZm9yZ2Vyb2NrLWFtLnNlcnZpY2UuY29yZS1jb21wdXRlLWlkYW0tZGVtby5pbnRlcm5hbDo4NDQzL29wZW5hbS9vYXV0aDIvaG1jdHMiLCJ0b2tlbk5hbWUiOiJhY2Nlc3NfdG9rZW4iLCJ0b2tlbl90eXBlIjoiQmVhcmVyIiwiYXV0aEdyYW50SWQiOiI2YWRlMTU3MS02Yjc5LTRlNGYtOTZjYS0yNGU5NTc2MTg5ZGQiLCJhdWQiOiJyZC1wcm9mZXNzaW9uYWwtYXBpIiwibmJmIjoxNTc1MzY1NDI0LCJncmFudF90eXBlIjoicGFzc3dvcmQiLCJzY29wZSI6WyJvcGVuaWQiLCJwcm9maWxlIiwicm9sZXMiLCJjcmVhdGUtdXNlciIsIm1hbmFnZS11c2VyIiwic2VhcmNoLXVzZXIiXSwiYXV0aF90aW1lIjoxNTc1MzY1NDI0LCJyZWFsbSI6Ii9obWN0cyIsImV4cCI6MTU3NTM5NDIyNCwiaWF0IjoxNTc1MzY1NDI0LCJleHBpcmVzX2luIjoyODgwMCwianRpIjoiN2U2MGIwMDItN2U5ZC00YWZhLWE2YWMtZWY5OTBhNjI2OTM0In0.YRzi61IELcwDoUUtV1M4gxJtGAWdQyCwZLNuUutjBu0tQNJervtqMgmrDJHHZ58I07QIHqbxlGLpsOlK1PsSkNs9DIyITyltpt-88rOzVjhGop4xVVYN77JfqJibwjdLmEEk_6xmkeB_I6ybxcLkb9lD4dmilOUTnGfHCsoOTehANEccMeOdypUemX4IyOdsgh-ru7_LPJkWOmkaz4lQz7pZPqEwHNPphznRNHrnpHcySrDBJV-ur09SyQ52as1zFbBnYFOW_b5WHnmUZMh7iILPdll7G40ZbN0z7hKT7tv4jU1Gjt-yHN5WB9aTvcxdtGdW2b4AFJQNh-b-pTI1Eg");
-        mutableRequest.putHeader("ServiceAuthorization", "hbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZF91c2VyX3Byb2ZpbGVfYXBpIiwiZXhwIjoxNTc1Mzc5OTE2fQ.gIN7QFT53q9OEb-PksPflh2gX83ZwgSqf_mrcv4OwV6C2siZauh71DosEjkYa9NJJ4FKO93WsAapWX6PWZ4X-g");
 
-        param1 =  ((HttpServletRequest) mutableRequest).getHeader(
-                "Authorization");
-        param2 =  ((HttpServletRequest) mutableRequest).getHeader(
-                "ServiceAuthorization");
-
-        param1 = Objects.isNull(param1) ? "" : param1.replaceAll("[\n|\r|\t]", "_");
-        param2 = Objects.isNull(param2) ? "" : param2.replaceAll("[\n|\r|\t]", "_");
-
-        LOG.info(String.format(":Authorization Header:: %S", param1));
-        LOG.info(String.format(":ServiceAuthorization Header:: %S", param2));
         chain.doFilter(mutableRequest, response);
     }
 }
