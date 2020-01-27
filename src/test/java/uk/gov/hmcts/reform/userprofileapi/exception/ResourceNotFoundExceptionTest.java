c35c38b303efdd74e2f863f65486672ec8bd51e8
AdilOoze <adil.oozeerally@hmcts.net>
2020-01-27 11:58:12 +0000

    RDCC-872 added tests for untested classes

---
+++ src/test/java/uk/gov/hmcts/reform/userprofileapi/exception/ResourceNotFoundExceptionTest.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.userprofileapi.exception;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ResourceNotFoundExceptionTest {
+
+    @Test
+    public void should_create_exception_correctly() {
+        String message = "this-is-a-test-message";
+        ResourceNotFoundException exception = new ResourceNotFoundException(message);
+
+        assertThat(exception).hasMessage(message);
+        assertThat(exception).isInstanceOf(ResourceNotFoundException.class);
+    }
+
+}
---
+++ src/test/java/uk/gov/hmcts/reform/userprofileapi/resource/UserProfileIdentifierTest.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.userprofileapi.resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import uk.gov.hmcts.reform.userprofileapi.domain.enums.IdentifierName;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UserProfileIdentifierTest {
+
+    @Test
+    public void should_hold_values_after_creation() {
+        UserProfileIdentifier userProfileIdentifierWithOneValue = new UserProfileIdentifier(IdentifierName.EMAIL, "test_email@hmcts.net");
+        UserProfileIdentifier userProfileIdentifierWithMultipleValue = new UserProfileIdentifier(IdentifierName.UUID_LIST, new ArrayList<>(Arrays.asList("UUID1", "UUID2", "UUID3")));
+
+        assertThat(userProfileIdentifierWithOneValue.getName()).isEqualTo(IdentifierName.EMAIL);
+        assertThat(userProfileIdentifierWithOneValue.getValue()).isEqualTo("test_email@hmcts.net");
+
+        assertThat(userProfileIdentifierWithMultipleValue.getName()).isEqualTo(IdentifierName.UUID_LIST);
+        assertThat(userProfileIdentifierWithMultipleValue.getValues()).contains("UUID1");
+        assertThat(userProfileIdentifierWithMultipleValue.getValues()).contains("UUID2");
+        assertThat(userProfileIdentifierWithMultipleValue.getValues()).contains("UUID3");
+    }
+
+}
