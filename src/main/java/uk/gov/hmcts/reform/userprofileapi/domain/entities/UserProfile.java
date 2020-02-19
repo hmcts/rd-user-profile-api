78dfc79cfabc904db699bba7a4dfdf536ccd1d1c
ConorH994 <conor.halpin@hmcts.net>
2020-02-19 10:56:59 +0000

    added subselect to the lists contained in user profile object

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/entities/UserProfile.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/entities/UserProfile.java
@@ -19,6 +19,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.http.HttpStatus;
 import uk.gov.hmcts.reform.userprofileapi.domain.IdamRolesInfo;
@@ -78,9 +80,11 @@ public class UserProfile {
     @UpdateTimestamp
     private LocalDateTime lastUpdated;
 
+    @Fetch(FetchMode.SUBSELECT)
     @OneToMany(mappedBy = "userProfile")
     private List<Audit> responses = new ArrayList<>();
 
+    @Fetch(FetchMode.SUBSELECT)
     @Transient
     private List<String> roles = new ArrayList<>();
 
