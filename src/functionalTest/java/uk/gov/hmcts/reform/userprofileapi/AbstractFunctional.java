3857e7d7abbea05a53f0919282a3e063b96e0392
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-05-12 18:22:18 +0100

    fix : (RDCC-1069) Sonar issue fixed

--- src/functionalTest/java/uk/gov/hmcts/reform/userprofileapi/AbstractFunctional.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/userprofileapi/AbstractFunctional.java
@@ -8,7 +8,6 @@ import io.restassured.RestAssured;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.serenitybdd.rest.SerenityRest;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
