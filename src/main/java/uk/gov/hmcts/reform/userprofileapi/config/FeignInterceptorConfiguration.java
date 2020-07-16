fe722fed9086b3a328b9da1ca89538a113c5aebc
AdilOoze <adil.oozeerally@hmcts.net>
2020-07-16 11:48:05 +0100

    RDCC-1583: committing to trigger travis builds

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/config/FeignInterceptorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/config/FeignInterceptorConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
-
 @Slf4j
 public class FeignInterceptorConfiguration {
 
