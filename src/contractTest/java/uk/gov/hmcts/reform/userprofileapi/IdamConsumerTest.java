8e625ab26a93fb70eb92140bb1b52d5687dc58b6
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-08-25 12:05:10 +0100

    feat:code committed related to user registration and /o/userinfo

--- src/contractTest/java/uk/gov/hmcts/reform/userprofileapi/IdamConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/userprofileapi/IdamConsumerTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import au.com.dius.pact.consumer.MockServer;
 import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.dsl.DslPart;
-import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
@@ -13,19 +12,14 @@ import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.model.RequestResponsePact;
 import com.google.common.collect.Maps;
 import groovy.util.logging.Slf4j;
-import io.restassured.RestAssured;
-import io.restassured.config.EncoderConfig;
-import io.restassured.parsing.Parser;
 import io.restassured.response.Response;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.UUID;
 
 import net.serenitybdd.rest.SerenityRest;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.HttpHeaders;
@@ -46,24 +40,92 @@ public class IdamConsumerTest {
     private static final String ROLES = "roles";
 
     private static final String IDAM_POST_USER_REGISTRATION_URL = "/api/v1/users/registration";
-    private static final String IDAM_USER_BY_ID_URL = "/api/v1/users/" + ID;
-    private static final String IDAM_USER_ROLES_BY_ID_URL = "/api/v1/users/" + ID + "/roles";
-    private static final String ACCESS_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiMWVyMFdSd2dJT1RBRm9q"
-            + "TRyQy9mYmVLdTNJPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJzb3VyYXYuYmhhdHRhY2hhcnlhQGhtY3RzLm5ldCIsImN0cyI6Ik9BV"
-            + "RVRIMl9TVEFURUxFU1NfR1JBTlQiLCJhdXRoX2xldmVsIjowLCJhdWRpdFRyYWNraW5nSWQiOiJhYjhlM2VlNi02ZjE1LTQ1MjItOTQz"
-            + "NC0yYzY0ZGJmZDcwYzAtMTI5MDg2NzIiLCJpc3MiOiJodHRwczovL2Zvcmdlcm9jay1hbS5zZXJ2aWNlLmNvcmUtY29tcHV0ZS1pZGFt"
-            + "LWFhdDIuaW50ZXJuYWw6ODQ0My9vcGVuYW0vb2F1dGgyL2htY3RzIiwidG9rZW5OYW1lIjoiYWNjZXNzX3Rva2VuIiwidG9rZW5fdHlw"
-            + "ZSI6IkJlYXJlciIsImF1dGhHcmFudElkIjoidXBXUk5tVkI3dEd2blJhNkUtZ1RnaFNGUDNzIiwiYXVkIjoicmQtcHJvZmVzc2lvbmFs"
-            + "LWFwaSIsIm5iZiI6MTU5MzAxODI3MywiZ3JhbnRfdHlwZSI6InBhc3N3b3JkIiwic2NvcGUiOlsib3BlbmlkIiwicHJvZmlsZSIsInJv"
-            + "bGVzIiwiY3JlYXRlLXVzZXIiLCJtYW5hZ2UtdXNlciIsInNlYXJjaC11c2VyIl0sImF1dGhfdGltZSI6MTU5MzAxODI3MywicmVhbG0i"
-            + "OiIvaG1jdHMiLCJleHAiOjE1OTMwNDcwNzMsImlhdCI6MTU5MzAxODI3MywiZXhwaXJlc19pbiI6Mjg4MDAsImp0aSI6IlBrdXZPS1Va"
-            + "Q1pDNmNHb19fcHRBV18tSE1CMCJ9.SOPRnE4GgQuoHA3jfnmhaClGzImQbJKPzlXuoT0TgaPa9RGk0oxFmBlPbniE1WquUPfiD1MJq-8"
-            + "TYbyW8mp0f7rMV11d3JuBezHbWvl1CWTY8CFB3UP-SWQdVcTioxci7jlib8klFo2fwnq3B1F73VybRxZ5h4EZe6ENvSFXKyW_EheJOLD"
-            + "mpmuS-0-DLy8O7rgVfWBiuSx9pn6kXkZgC3yRqakgN6d22oP8iIe1YnarDrmb2XTuPNogNCzlTeTfpQ1aY66VQkcr_cS4FKOMP1EtFh9"
-            + "b9SaZd3FcYbbCZk4IB0AkDkPdFE-1bCa2COIYbAI0bPNssx9fRqT4E2aUQg";
+    private static final String IDAM_USER_BY_EMAIL = "/api/v1/users";
+    private static final String IDAM_USERINFO_URL = "/o/userinfo";
+
+    private static final String ACCESS_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdeRre";
 
 
-    @Pact(provider = "Idam_api", consumer = "rd_user_profile_api__Idam_api")
+    @Pact(provider = "Idam_api", consumer = "rd_user_profile_api")
+    public RequestResponsePact executeGetUserInfoDetailsAndGet200(PactDslWithProvider builder) {
+
+        Map<String, String> requestHeaders = Maps.newHashMap();
+        requestHeaders.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+        Map<String, Object> params = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+
+        params.put("redirect_uri", "http://www.dummy-pact-service.com/callback");
+        params.put("client_id", "pact");
+        params.put("client_secret", "pactsecret");
+        params.put("scope", "openid profile roles");
+        params.put("username", "prdadmin@email.net");
+        params.put("password", "Password123");
+
+        Map<String, String> responseheaders = Maps.newHashMap();
+        responseheaders.put("Content-Type", "application/json");
+
+        return builder
+                .given("I have obtained an access_token as a user",params)
+                .uponReceiving("Provider returns user info to RD - USER PROFILE API")
+                .path(IDAM_USERINFO_URL)
+                .headers("Authorization",ACCESS_TOKEN)
+                .method(HttpMethod.GET.toString())
+                .willRespondWith()
+                .status(HttpStatus.OK.value())
+                .headers(responseheaders)
+                .body(createUserInfoResponse())
+                .toPact();
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "executeGetUserInfoDetailsAndGet200")
+    public void should_get_user_info_details_with_access_token(MockServer mockServer) throws JSONException {
+
+        Map<String, String> headers = Maps.newHashMap();
+        headers.put(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN);
+        headers.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+
+        String detailsResponseBody =
+                SerenityRest
+                        .given()
+                        .headers(headers)
+                        .when()
+                        .get(mockServer.getUrl() + IDAM_USERINFO_URL)
+                        .then()
+                        .statusCode(200)
+                        .and()
+                        .extract()
+                        .body()
+                        .asString();
+
+        JSONObject response = new JSONObject(detailsResponseBody);
+
+        assertThat(detailsResponseBody).isNotNull();
+        assertThat(response).hasNoNullFieldsOrProperties();
+        assertThat(response.getString("uid")).isNotBlank();
+        assertThat(response.getString("given_name")).isNotBlank();
+        assertThat(response.getString("family_name")).isNotBlank();
+        JSONArray rolesArr = response.getJSONArray("roles");
+        assertThat(rolesArr).isNotNull();
+        assertThat(rolesArr.length()).isNotZero();
+        assertThat(rolesArr.get(0).toString()).isNotBlank();
+
+    }
+
+    private DslPart createUserInfoResponse() {
+
+        return new PactDslJsonBody()
+                .stringType("uid", "1111-2222-3333-4567")
+                .stringType("given_name", "puiCaseManager")
+                .stringType("family_name", "Jar")
+                .array("roles")
+                .stringType("prd-admin")
+                .stringType("IDAM_ADMIN_USER")
+                .closeArray();
+
+    }
+
+
+    @Pact(provider = "Idam_api", consumer = "rd_user_profile_api")
     public RequestResponsePact executePostRegistrationAndGet200(PactDslWithProvider builder) {
 
         Map<String, String> headers = Maps.newHashMap();
@@ -136,52 +198,4 @@ public class IdamConsumerTest {
         assertThat(actualResponseBody.getStatusCode()).isEqualTo(200);
     }
 
-
-    private PactDslJsonBody createUserProfileRequest() {
-        boolean status = true;
-        PactDslJsonArray array = new PactDslJsonArray()
-                .string("pui-organisation-manager")
-                .string("pui-case-manager");
-        return new PactDslJsonBody()
-                .stringType("email", "rdup@spookmail.com")
-                .stringType(FIRST_NAME, "up")
-                .stringType("id", UUID.randomUUID().toString())
-                .stringType(LAST_NAME, "rd")
-                .stringType(ROLES, array.toString());
-    }
-
-
-    private DslPart createUserResponse() {
-        PactDslJsonArray array = new PactDslJsonArray()
-                .string("pui-organisation-manager")
-                .string("pui-case-manager");
-
-        return PactDslJsonArray.arrayEachLike(1)
-                .stringType("id", ID)
-                .stringType("email", EMAIL)
-                .stringType("forename", "Seymore")
-                .stringType("surname", "Skinner")
-                .object("roles", array)
-                .booleanType("pending", false)
-                .stringType("repsonseStatusCode", HttpStatus.OK.toString())
-                .stringType("statusMessage", "11 OK")
-                .closeObject();
-    }
-
-    private PactDslJsonBody createUserResponseRoleDeleted() {
-        PactDslJsonArray array = new PactDslJsonArray()
-                .string("pui-organisation-manager");
-
-        return new PactDslJsonBody()
-                .stringType("id", ID)
-                .stringType("email", EMAIL)
-                .stringType("forename", "Seymore")
-                .stringType("surname", "Skinner")
-                .object("roles", array)
-                .booleanType("active", true)
-                .booleanType("pending", false)
-                .stringType("repsonseStatusCode", HttpStatus.OK.toString())
-                .stringType("statusMessage", "11 OK");
-
-    }
 }
