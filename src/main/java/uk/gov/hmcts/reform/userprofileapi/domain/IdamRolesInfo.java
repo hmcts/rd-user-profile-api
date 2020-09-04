9b91e387e90ee7e8852466adda493fbda5bbcbd9
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-09-04 09:16:10 +0100

    feat:sonarqube issues fix

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/IdamRolesInfo.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/domain/IdamRolesInfo.java
@@ -26,15 +26,13 @@ public class IdamRolesInfo {
     public IdamRolesInfo(ResponseEntity<Object> entity) {
         if (nonNull(entity)  && nonNull(entity.getBody()) && entity.getBody() instanceof IdamUserResponse) {
             IdamUserResponse idamUserResponse = (IdamUserResponse) entity.getBody();
-            if (null != idamUserResponse) {
-                this.id = idamUserResponse.getId();
-                this.roles = idamUserResponse.getRoles();
-                this.email = idamUserResponse.getEmail();
-                this.forename = idamUserResponse.getForename();
-                this.surname = idamUserResponse.getSurname();
-                this.active = idamUserResponse.getActive();
-                this.pending = idamUserResponse.getPending();
-            }
+            this.id = idamUserResponse.getId();
+            this.roles = idamUserResponse.getRoles();
+            this.email = idamUserResponse.getEmail();
+            this.forename = idamUserResponse.getForename();
+            this.surname = idamUserResponse.getSurname();
+            this.active = idamUserResponse.getActive();
+            this.pending = idamUserResponse.getPending();
 
         }
         loadStatusCodes(entity);
