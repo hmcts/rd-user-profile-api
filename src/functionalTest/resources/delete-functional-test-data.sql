853df94e78e48180912a87f63c9fad637359b7b4
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-09-18 14:13:17 +0100

    feat: RDCC-1739 Removing mailinator domain until IAC replace their domain.

--- src/functionalTest/resources/delete-functional-test-data.sql
+++ src/functionalTest/resources/delete-functional-test-data.sql
@@ -1,4 +1,4 @@
 delete from user_profile
-where email_address like any (values('%@prdfunctestuser.com'), ('%@mailinator.com'));
+where email_address like any (values('%@prdfunctestuser.com'));
 
 commit;
