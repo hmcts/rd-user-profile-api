f6d0b20e5e255df28b6c5a5c5cbd9674802d970d
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-05 14:51:29 +0100

    Removed Ignore namespace

--- src/integrationTest/java/uk/gov/hmcts/reform/userprofileapi/integration/RetrieveMultipleUserProfilesIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/userprofileapi/integration/RetrieveMultipleUserProfilesIntTest.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
