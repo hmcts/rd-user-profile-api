97495aa0695b118b785de5e252d17c5776407b18
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-05 11:31:11 +0100

    Commit TF upgrade to 0.12.16 and AzureRM 2.20.0

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,113 +1,61 @@
-# Temporary fix for template API version error on deployment
-provider "azurerm" {
-  version = "1.22.0"
-}
-
 locals {
-  local_env = "${(var.env == "preview" || var.env == "spreview") ? (var.env == "preview" ) ? "aat" : "saat" : var.env}"
-  preview_app_service_plan = "${var.product}-${var.component}-${var.env}"
-  non_preview_app_service_plan = "${var.product}-${var.env}"
-  app_service_plan = "${var.env == "preview" || var.env == "spreview" ? local.preview_app_service_plan : local.non_preview_app_service_plan}"
-
-  preview_vault_name = "${var.raw_product}-aat"
-  non_preview_vault_name = "${var.raw_product}-${var.env}"
-  key_vault_name = "${var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name}"
-
-  s2s_url = "http://rpe-service-auth-provider-${local.local_env}.service.core-compute-${local.local_env}.internal"
-  s2s_vault_name = "s2s-${local.local_env}"
-  s2s_vault_uri = "https://s2s-${local.local_env}.vault.azure.net/"
-  idam_url = "${var.env == "prod" ? "https://idam-api.platform.hmcts.net" : "https://idam-api.${local.local_env}.platform.hmcts.net" }"
-  OIDC_ISSUER_URL = "https://forgerock-am.service.core-compute-idam-{{ .Values.global.environment }}.internal:8443/openam/oauth2/hmcts"
-  OPEN_ID_API_BASE_URI = "https://idam-web-public.{{ .Values.global.environment }}.platform.hmcts.net/o"
+  preview_vault_name      = join("-", [var.raw_product, "aat"])
+  non_preview_vault_name  = join("-", [var.raw_product, ver.env])
+  key_vault_name          = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = "${var.product}-${var.component}-${var.env}"
-    location = "${var.location}"
-    tags {
-      "Deployment Environment" = "${var.env}"
-      "Team Name" = "${var.team_name}"
-      "lastUpdated" = "${timestamp()}"
+  name      = join("-", [var.raw_product, var.component, var.env])
+  location  = var.location
+  tags      = {
+      "Deployment Environment" = var.env
+      "Team Name" = var.team_name
+      "lastUpdated" = timestamp()
     }
 }
 
 data "azurerm_key_vault" "rd_key_vault" {
-  name = "${local.key_vault_name}"
-  resource_group_name = "${local.key_vault_name}"
-}
-
-data "azurerm_key_vault" "s2s_key_vault" {
-  name = "s2s-${local.local_env}"
-  resource_group_name = "rpe-service-auth-provider-${local.local_env}"
-}
-
-data "azurerm_key_vault_secret" "up_s2s_microservice" {
-  name = "up-s2s-microservice"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "s2s_url" {
-  name = "s2s-url"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "idam_url" {
-  name = "idam-url"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "OIDC_ISSUER_URL" {
-  name = "OIDC-ISSUER-URL"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "OPEN_ID_API_BASE_URI" {
-  name = "OPEN-ID-API-BASE-URI"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
-}
-
-data "azurerm_key_vault_secret" "up_s2s_secret" {
-  name = "microservicekey-rd-user-profile-api"
-  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
+  name                = local.key_vault_name
+  resource_group_name = local.key_vault_name
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name      = "${var.component}-POSTGRES-USER"
-  value     = "${module.db-user-profile.user_name}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-USER"])
+  value         = module.db-user-profile.user_name
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name      = "${var.component}-POSTGRES-PASS"
-  value     = "${module.db-user-profile.postgresql_password}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-PASS"])
+  value         = module.db-user-profile.postgresql_password
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name      = "${var.component}-POSTGRES-HOST"
-  value     = "${module.db-user-profile.host_name}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-HOST"])
+  value         = module.db-user-profile.host_name
+  key_vault_id  = {data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  name      = "${var.component}-POSTGRES-PORT"
-  value     = "5432"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-PORT"])
+  value         = "5432"
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name      = "${var.component}-POSTGRES-DATABASE"
-  value     = "${module.db-user-profile.postgresql_database}"
-  key_vault_id = "${data.azurerm_key_vault.rd_key_vault.id}"
+  name          = join("-", [var.component, "POSTGRES-DATABASE"])
+  value         = module.db-user-profile.postgresql_database
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 module "db-user-profile" {
-  source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product = "${var.product}-${var.component}-postgres-db"
-  location = "${var.location}"
-  subscription = "${var.subscription}"
-  env = "${var.env}"
-  postgresql_user = "dbuserprofile"
-  database_name = "dbuserprofile"
-  common_tags = "${var.common_tags}"
+  source            = "git@github.com:hmcts/cnp-module-postgres?ref=master"
+  product           = join("-", [var.product, var.component, "postgres-db"])
+  location          = var.location
+  subscription      = var.subscription
+  env               = var.env
+  postgresql_user   = "dbuserprofile"
+  database_name     = "dbuserprofile"
+  common_tags       = var.common_tags
 }
