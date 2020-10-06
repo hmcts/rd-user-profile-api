d505482c0fa68af2e8d2a8c5a40a1edbfa343c33
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2020-10-06 11:06:03 +0100

    TF upgrade to to 0.12.16 & Azure RM Upgrade to 2.20.0 - Master (#519)
    * code committed to fix functional test case
    
    * refactor:pref test set up
    
    * sonarqube issues fixing
    
    * feat:iac and fpl names added in the authorised services list
    
    * feat:jenkins issues fixing
    
    * feat:jenkins issues fixing
    
    * feat:suppression xml copied from demo
    
    * feat:suppression xml updated to fix the build issues
    
    * feat:fixing integration test issues
    
    * Merge branch 'perftest' of https://github.com/hmcts/rd-user-profile-api into perftest
    
    # Conflicts:
    #	Jenkinsfile_CNP
    #	build.gradle
    #	config/owasp/suppressions.xml
    #	src/main/java/uk/gov/hmcts/reform/userprofileapi/controller/UserProfileController.java
    #	src/main/resources/application.yaml
    #	src/test/java/uk/gov/hmcts/reform/userprofileapi/controller/UserProfileControllerTest.java
    
    * commit the file to show appinsights
    
    * commit the file to show appinsights
    
    * Resolved merge conflicts between master and demo
    
    * Removing unused class
    
    * feat:  adding the response class
    
    * feat:  fixing the changes to build
    
    * feat:  fixing the changes to build
    
    * Commit TF upgrade to 0.12.16 and AzureRM 2.20.0
    
    * Fixed syntax issue
    
    * Fix syntax issues
    
    * Removed @Ignore
    
    * Removed Ignore namespace
    
    Co-authored-by: KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
    Co-authored-by: KOTLAPRA23 <prashanth.kotla@hmcts.net>
    Co-authored-by: abhijit <Abhijit.diwan@hmcts.net>
    Co-authored-by: pksaxena01 <50022039+pksaxena01@users.noreply.github.com>
    Co-authored-by: abhijitdiwan <49564418+abhijitdiwan@users.noreply.github.com>
    Co-authored-by: AdilOoze <adil.oozeerally@hmcts.net>
    Co-authored-by: Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,74 +1,83 @@
 variable "product" {
-  type = "string"
+  type = string
 }
 
 variable "raw_product" {
+  type    = string
   default = "rd" // jenkins-library overrides product for PRs and adds e.g. pr-123-rd
 }
 
 variable "component" {
-  type = "string"
+  type = string
 }
 
 variable "location" {
-  type    = "string"
+  type    = string
   default = "UK South"
 }
 
 variable "env" {
-  type = "string"
+  type = string
 }
 
 variable "subscription" {
-  type = "string"
+  type = string
 }
 
-variable "ilbIp" {}
+variable "ilbIp" {
+  type = string
+}
 
 variable "common_tags" {
-  type = "map"
+  type = map(string)
 }
 
 variable "capacity" {
+  type    = string
   default = "1"
 }
 
 variable "instance_size" {
+  type    = string
   default = "I1"
 }
 
 variable "postgresql_user" {
+  type    = string
   default = "dbuserprofile"
 }
 
 variable "database_name" {
+  type    = string
   default = "dbuserprofile"
 }
 
 variable "appinsights_instrumentation_key" {
+  type    = string
   default = ""
 }
 
 variable "root_logging_level" {
+  type    = string
   default = "INFO"
 }
 
 variable "log_level_spring_web" {
+  type    = string
   default = "INFO"
 }
 
 variable "log_level_rd" {
+  type    = string
   default = "INFO"
 }
 
 variable "team_name" {
-  default     = "RD"
+  type    = string
+  default = "RD"
 }
 
 variable "managed_identity_object_id" {
+  type    = string
   default = ""
-}
-
-variable "enable_ase" {
-  default = false
 }
