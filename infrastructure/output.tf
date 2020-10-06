29b102d40d75713375d55629cde5cd1a9109b565
Vijay Kumar Rajagopalan <62386423+vijayrajagopalan-hmcts@users.noreply.github.com>
2020-10-06 09:34:14 +0100

    TF upgrade to to 0.12.16 & Azure RM Upgrade to 2.20.0 - Demo (#515)
    * code committed to fix functional test case
    
    * refactor:pref test set up
    
    * sonarqube issues fixing
    
    * feat:iac and fpl names added in the authorised services list
    
    * feat:jenkins issues fixing
    
    * feat:jenkins issues fixing
    
    * feat:suppression xml copied from demo
    
    * feat:suppression xml updated to fix the build issues
    
    * feat:fixing integration test issues
    
    * Merge branch 'perftest' of https://github.com/hmcts/rd-user-profile-api into perftest
    
    # Conflicts:
    #	Jenkinsfile_CNP
    #	build.gradle
    #	config/owasp/suppressions.xml
    #	src/main/java/uk/gov/hmcts/reform/userprofileapi/controller/UserProfileController.java
    #	src/main/resources/application.yaml
    #	src/test/java/uk/gov/hmcts/reform/userprofileapi/controller/UserProfileControllerTest.java
    
    * commit the file to show appinsights
    
    * commit the file to show appinsights
    
    * feat: RDCC-1739 Refactored code to initialise connection in predestoy method
    
    * Commit TF upgrade to 0.12.16 and AzureRM 2.20.0
    
    * Fixed syntax issue
    
    * Fix syntax issues
    
    * Removed @Ignore
    
    * Removed Ignore namespace
    
    Co-authored-by: KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
    Co-authored-by: KOTLAPRA23 <prashanth.kotla@hmcts.net>
    Co-authored-by: abhijit <Abhijit.diwan@hmcts.net>
    Co-authored-by: pksaxena01 <50022039+pksaxena01@users.noreply.github.com>
    Co-authored-by: abhijitdiwan <49564418+abhijitdiwan@users.noreply.github.com>
    Co-authored-by: shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
    Co-authored-by: shreedhar-hmcts <49564702+shreedhar-hmcts@users.noreply.github.com>

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,12 +1,3 @@
 output "resourceGroup" {
-  value = "${azurerm_resource_group.rg.name}"
-}
-
-output "appServicePlan" {
-  value = "${local.app_service_plan}"
-}
-
-
-output "vaultUri" {
-  value = "${local.s2s_vault_uri}"
+  value = azurerm_resource_group.rg.name
 }
