a403444919fbe7e15894f57307f2e6582237dff9
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-07 15:57:48 +0100

    TF Upgrade to 13.3 and Azure RM to 2.25+

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -24,60 +24,60 @@ variable "subscription" {
   type = string
 }
 
-variable "ilbIp" {
-  type = string
-}
+# variable "ilbIp" {
+#   type = string
+# }
 
 variable "common_tags" {
   type = map(string)
 }
 
-variable "capacity" {
-  type    = string
-  default = "1"
-}
+# variable "capacity" {
+#   type    = string
+#   default = "1"
+# }
 
-variable "instance_size" {
-  type    = string
-  default = "I1"
-}
+# variable "instance_size" {
+#   type    = string
+#   default = "I1"
+# }
 
-variable "postgresql_user" {
-  type    = string
-  default = "dbuserprofile"
-}
+# variable "postgresql_user" {
+#   type    = string
+#   default = "dbuserprofile"
+# }
 
-variable "database_name" {
-  type    = string
-  default = "dbuserprofile"
-}
+# variable "database_name" {
+#   type    = string
+#   default = "dbuserprofile"
+# }
 
-variable "appinsights_instrumentation_key" {
-  type    = string
-  default = ""
-}
+# variable "appinsights_instrumentation_key" {
+#   type    = string
+#   default = ""
+# }
 
-variable "root_logging_level" {
-  type    = string
-  default = "INFO"
-}
+# variable "root_logging_level" {
+#   type    = string
+#   default = "INFO"
+# }
 
-variable "log_level_spring_web" {
-  type    = string
-  default = "INFO"
-}
+# variable "log_level_spring_web" {
+#   type    = string
+#   default = "INFO"
+# }
 
-variable "log_level_rd" {
-  type    = string
-  default = "INFO"
-}
+# variable "log_level_rd" {
+#   type    = string
+#   default = "INFO"
+# }
 
 variable "team_name" {
   type    = string
   default = "RD"
 }
 
-variable "managed_identity_object_id" {
-  type    = string
-  default = ""
-}
+# variable "managed_identity_object_id" {
+#   type    = string
+#   default = ""
+# }
