89655a19d5b08704ea448ca8d4e765cf5394b249
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-11-02 16:04:41 +0000

    Delete secrets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,40 +19,40 @@ data "azurerm_key_vault" "rd_key_vault" {
   resource_group_name = local.key_vault_name
 }
 
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  provider      = azurerm.azure-1
-  name          = join("-", [var.component, "POSTGRES-USER"])
-  value         = module.db-user-profile.user_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
+# resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+#   provider      = azurerm.azure-1
+#   name          = join("-", [var.component, "POSTGRES-USER"])
+#   value         = module.db-user-profile.user_name
+#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+# }
 
-resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  provider      = azurerm.azure-1
-  name          = join("-", [var.component, "POSTGRES-PASS"])
-  value         = module.db-user-profile.postgresql_password
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
+# resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+#   provider      = azurerm.azure-1
+#   name          = join("-", [var.component, "POSTGRES-PASS"])
+#   value         = module.db-user-profile.postgresql_password
+#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+# }
 
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  provider      = azurerm.azure-1
-  name          = join("-", [var.component, "POSTGRES-HOST"])
-  value         = module.db-user-profile.host_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
+# resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+#   provider      = azurerm.azure-1
+#   name          = join("-", [var.component, "POSTGRES-HOST"])
+#   value         = module.db-user-profile.host_name
+#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+# }
 
-resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-  provider      = azurerm.azure-1
-  name          = join("-", [var.component, "POSTGRES-PORT"])
-  value         = "5432"
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
+# resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+#   provider      = azurerm.azure-1
+#   name          = join("-", [var.component, "POSTGRES-PORT"])
+#   value         = "5432"
+#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+# }
 
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  provider      = azurerm.azure-1
-  name          = join("-", [var.component, "POSTGRES-DATABASE"])
-  value         = module.db-user-profile.postgresql_database
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
+# resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+#   provider      = azurerm.azure-1
+#   name          = join("-", [var.component, "POSTGRES-DATABASE"])
+#   value         = module.db-user-profile.postgresql_database
+#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+# }
 
 module "db-user-profile" {
   source            = "git@github.com:hmcts/cnp-module-postgres?ref=master"
