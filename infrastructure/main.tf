7eaa18e3f2983cb141dd6513fb55ea6fd1896d07
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-12-17 10:44:05 +0000

    Create s2s secret

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,6 +2,10 @@ locals {
   preview_vault_name      = join("-", [var.raw_product, "aat"])
   non_preview_vault_name  = join("-", [var.raw_product, var.env])
   key_vault_name          = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
+
+  s2s_rg_prefix               = "rpe-service-auth-provider"
+  s2s_key_vault_name          = var.env == "preview" || var.env == "spreview" ? join("-", ["s2s", "aat"]) : join("-", ["s2s", var.env])
+  s2s_vault_resource_group    = var.env == "preview" || var.env == "spreview" ? join("-", [local.s2s_rg_prefix, "aat"]) : join("-", [local.s2s_rg_prefix, var.env])
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -17,6 +21,22 @@ resource "azurerm_resource_group" "rg" {
 data "azurerm_key_vault" "rd_key_vault" {
   name                = local.key_vault_name
   resource_group_name = local.key_vault_name
+}
+
+data "azurerm_key_vault" "s2s_key_vault" {
+  name                = local.s2s_key_vault_name
+  resource_group_name = local.s2s_vault_resource_group
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  name          = "microservicekey-rd-user-profile-api"
+  key_vault_id  = data.azurerm_key_vault.s2s_key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "user_profile_s2s_secret" {
+  name          = "user-profile-api-s2s-secret"
+  value         = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER" {
