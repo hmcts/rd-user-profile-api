40fc71b54c77914c07192c7257d4b92bf9fe2e46
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2021-01-06 09:57:38 +0000

    Minor updates

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,9 +3,9 @@ locals {
   non_preview_vault_name  = join("-", [var.raw_product, var.env])
   key_vault_name          = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
 
-  s2s_rg_prefix               = "rpe-service-auth-provider"
-  s2s_key_vault_name          = var.env == "preview" || var.env == "spreview" ? join("-", ["s2s", "aat"]) : join("-", ["s2s", var.env])
-  s2s_vault_resource_group    = var.env == "preview" || var.env == "spreview" ? join("-", [local.s2s_rg_prefix, "aat"]) : join("-", [local.s2s_rg_prefix, var.env])
+  s2s_rg_prefix             = "rpe-service-auth-provider"
+  s2s_key_vault_name        = var.env == "preview" || var.env == "spreview" ? join("-", ["s2s", "aat"]) : join("-", ["s2s", var.env])
+  s2s_vault_resource_group  = var.env == "preview" || var.env == "spreview" ? join("-", [local.s2s_rg_prefix, "aat"]) : join("-", [local.s2s_rg_prefix, var.env])
 }
 
 resource "azurerm_resource_group" "rg" {
