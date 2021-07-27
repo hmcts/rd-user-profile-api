e2fab2d233c47b2f9330464f659be26226ad87b8
AdilOoze <adil.oozeerally@hmcts.net>
2021-07-27 09:36:28 +0100

    RDCC-2820: amending Integration Tests in RetrieveUserProfileIntTest

--- src/integrationTest/java/uk/gov/hmcts/reform/userprofileapi/integration/RetrieveUserProfileIntTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/userprofileapi/integration/RetrieveUserProfileIntTest.java
@@ -129,11 +129,12 @@ public class RetrieveUserProfileIntTest extends AuthorizationEnabledIntegrationT
         UserProfile userProfile = userProfileMap.get("user");
 
         UserProfileWithRolesResponse retrievedResource =
-                userProfileRequestHandlerTest.sendGet(
+                userProfileRequestHandlerTest.sendGetFromHeader(
                         mockMvc,
-                        APP_BASE_PATH + SLASH + "roles" + "?" + "email=" + userProfile.getEmail(),
+                        APP_BASE_PATH + SLASH + "roles",
                         OK,
-                        UserProfileWithRolesResponse.class
+                        UserProfileWithRolesResponse.class,
+                        userProfile.getEmail()
                 );
 
         assertThat(retrievedResource).isNotNull();
@@ -329,22 +330,14 @@ public class RetrieveUserProfileIntTest extends AuthorizationEnabledIntegrationT
     }
 
     @Test
-    public void should_return_400_when_query_with_no_email_in_header_and_query_param() throws Exception {
-        userProfileRequestHandlerTest.sendGet(
-                mockMvc,
-                APP_BASE_PATH,
-                BAD_REQUEST
-        );
-    }
-
-    @Test
     public void should_return_badRequest_when_retrieve_userprofile_with_roles_by_no_email() throws Exception {
 
         MvcResult retrievedResource =
-                userProfileRequestHandlerTest.sendGet(
+                userProfileRequestHandlerTest.sendGetFromHeader(
                         mockMvc,
                         APP_BASE_PATH + SLASH + "roles",
-                        BAD_REQUEST
+                        NOT_FOUND,
+                        ""
                 );
 
         assertThat(retrievedResource).isNotNull();
@@ -352,7 +345,8 @@ public class RetrieveUserProfileIntTest extends AuthorizationEnabledIntegrationT
         Exception resolvedException = retrievedResource.getResolvedException();
 
         assertThat(resolvedException).isNotNull();
-        assertThat(resolvedException.getMessage()).isEqualTo("No User Email provided via header or param");
+        assertThat(resolvedException.getMessage())
+                .isEqualTo("Could not find resource from database with given identifier");
 
     }
 }
