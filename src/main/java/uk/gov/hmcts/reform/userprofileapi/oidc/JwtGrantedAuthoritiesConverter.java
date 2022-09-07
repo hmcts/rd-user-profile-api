71b7b0beb0d77de159fe7138a2568a4de29a1051
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-09-07 13:41:21 +0100

    Adding logging level

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/oidc/JwtGrantedAuthoritiesConverter.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/oidc/JwtGrantedAuthoritiesConverter.java
@@ -45,7 +45,7 @@ public class JwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
         List<GrantedAuthority> authorities = new ArrayList<>();
-        log.debug("Inside JWT Granted Authorities Converter" + jwt.getClaim(TOKEN_NAME));
+        log.debug("Inside JWT Granted Authorities Converter");
         if (TRUE.equals(jwt.containsClaim(TOKEN_NAME)) && jwt.getClaim(TOKEN_NAME).equals(ACCESS_TOKEN)) {
             userInfo = idamRepository.getUserInfo(jwt.getTokenValue());
             log.debug("After fetching User Information from idam" + userInfo.getUid() + userInfo.getRoles());
