d4f74e7d7600804abe05f41073238ad7fa457773
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2022-09-15 19:36:46 +0100

    Update FeignInterceptorConfiguration.java

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/config/FeignInterceptorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/config/FeignInterceptorConfiguration.java
@@ -45,12 +45,4 @@ public class FeignInterceptorConfiguration {
             }
         };
     }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public Feign.Builder feignBuilder(Retryer r) {
-        return Feign.builder().retryer(r)
-                .client(new feign.okhttp.OkHttpClient());
-    }
-
 }
