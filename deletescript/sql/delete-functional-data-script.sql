599c27a4d6af7fa0efbc7d7c32b865fdbf6836cb
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-05-15 13:30:41 +0100

    [RDCC-6201]- Added script to delete test users created by SRD repo

--- deletescript/sql/delete-functional-data-script.sql
+++ deletescript/sql/delete-functional-data-script.sql
@@ -1,4 +1,7 @@
-delete from user_profile where email_address like any (values('%@prdfunctestuser.com'));
+delete from user_profile where
+        email_address like any (values('%@prdfunctestuser.com'))
+       or email_address like any (values('cwr-rd-func-test-user-only%'))
+       or email_address like any(values('staff-rd-profile-func-test-user-only-%'));
 
 commit;
 
