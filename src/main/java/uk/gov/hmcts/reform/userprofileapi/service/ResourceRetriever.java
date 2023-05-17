f6cfb649213255a0275da4bb2b98db1b14fd871d
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-05-17 16:25:41 +0100

    Update User Details Regardless of Active Status

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/service/ResourceRetriever.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/service/ResourceRetriever.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public interface ResourceRetriever<T extends RequestData> {
 
-    UserProfile retrieve(T identifier, boolean fetchRoles);
+    UserProfile retrieve(T identifier, boolean fetchRoles, String origin);
 
     List<UserProfile> retrieveMultipleProfiles(T identifier, boolean showDeleted, boolean rolesRequired);
 
