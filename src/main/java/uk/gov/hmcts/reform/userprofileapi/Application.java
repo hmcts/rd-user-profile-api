bd168b1c9adf3d28f3221547a0642fe1ea777790
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-12-12 23:20:10 +0000

    Fix application startup issue

--- src/main/java/uk/gov/hmcts/reform/userprofileapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/userprofileapi/Application.java
@@ -1,8 +1,10 @@
 package uk.gov.hmcts.reform.userprofileapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
@@ -16,6 +18,7 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 @EnableJpaRepositories
 @EnableRetry
 @EnableCaching
+@EnableAutoConfiguration(exclude = { WebMvcAutoConfiguration.class })
 @ImportAutoConfiguration({
         FeignAutoConfiguration.class
 })
