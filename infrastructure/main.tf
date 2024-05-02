a5533283ca93e517ded02c8d429c1c28bd07cf20
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-05-02 11:44:06 +0100

    Update schema ownership and force permission

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -102,7 +102,7 @@ module "db-user-profile-v16" {
     }
   ]
   # Setup Access Reader db user
-  force_user_permissions_trigger = "1"
+  force_user_permissions_trigger = "2"
 
   # Sets correct DB owner after migration to fix permissions
   enable_schema_ownership = var.enable_schema_ownership
