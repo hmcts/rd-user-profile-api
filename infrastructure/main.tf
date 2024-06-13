8004a1d6bec5d8a6259fbf164fa1d087d8024ea0
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-06-13 12:50:04 +0100

    DTSRD-2845: Resize PG flexible server instance

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -70,6 +70,7 @@ module "db-user-profile-v16" {
 
   subnet_suffix        = "expanded"
   pgsql_version        = "16"
+  pgsql_sku            = var.pgsql_sku
   product              = "rd"
   name               = join("-", [var.product-v16, var.component-v16])
 
